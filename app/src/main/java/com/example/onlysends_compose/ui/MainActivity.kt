5c38a8bdcfdf773923248da1ce044eedbc270aa8
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-04-14 12:17:19 -0400

    working on new tutorial for google OAuth in compose

---
+++ app/src/main/java/com/example/onlysends_compose/ui/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.onlysends_compose.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.example.onlysends_compose.R
+import com.example.onlysends_compose.presentation.sign_in.GoogleAuthUiCLient
+import com.example.onlysends_compose.presentation.sign_in.SignInScreen
+import com.example.onlysends_compose.presentation.sign_in.SignInViewModel
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val googleAuthUiClient by lazy {
+        GoogleAuthUiCLient(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "sign_in") {
+                composable("sign-in") {
+                    val viewModel = viewModel<SignInViewModel>()
+                    val state = viewModel.state.collectAsStateWithLifecycle()
+
+                    val launcher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.StartIntentSenderForResult(),
+                        onResult = { result ->
+                            if (result.resultCode == RESULT_OK) {
+                                lifecycleScope.launch {
+                                    val signInResult = googleAuthUiClient.signInWithIntent(
+                                        intent = result.data ?: return@launch
+                                    )
+                                    // pass the result to the viewModel so that the state gets properly updated
+                                    viewModel.onSignInResult(signInResult)
+                                }
+                            }
+                        }
+                    )
+
+                    SignInScreen(
+                        state = state.value,
+                        onSignInClick = {
+                            lifecycleScope.launch {
+                                val signInIntentSender = googleAuthUiClient.signIn()
+                                launcher.launch(
+                                    IntentSenderRequest.Builder(
+                                        signInIntentSender ?: return@launch
+                                    ).build()
+                                )
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+}
