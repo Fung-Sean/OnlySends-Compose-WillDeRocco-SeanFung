47a18e131424f92f6fb0671729a2884baac37987
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-04-15 22:20:07 -0400

    added name and climbStyle inputs to the profile page

--- app/src/main/java/com/example/onlysends_compose/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/onlysends_compose/ui/profile/ProfileScreen.kt
@@ -4,12 +4,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,18 +34,24 @@ import com.example.onlysends_compose.firestore.types.User
 
 // simple function to render the profile page (with sign-out options)
 // for now -> doesn't contain any state, so no need for ViewModel
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     user: User?,
     onSignOut: () -> Unit
 ) {
+    // dropdown menu of climbing styles
+    var expanded by remember { mutableStateOf(false) }
+    var climbStyle by remember { mutableStateOf("") }
+    val climbStyles = listOf("Bouldering", "Sport Climbing", "Trad Climbing", "Ice Climbing")
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // display profile picture
-        if(user?.profilePictureUrl != null) {
+        if (user?.profilePictureUrl != null) {
             AsyncImage(
                 model = user.profilePictureUrl,
                 contentDescription = "User profile picture",
@@ -44,18 +62,84 @@ fun ProfileScreen(
             )
             Spacer(modifier = Modifier.height(16.dp))
         }
-        // display username
-        if(user?.username != null) {
+
+        // Display "Full Name" text and input box in a Column
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // display "Full Name" text
             Text(
-                text = user.username,
-                textAlign = TextAlign.Center,
-                fontSize = 36.sp
+                text = "Full Name",
+                textAlign = TextAlign.Left,
+                fontSize = 18.sp
+            )
+
+            // input box for user's full name
+            OutlinedTextField(
+                value = user?.username ?: "",
+                onValueChange = { /*TODO*/ },
+                label = { user?.username ?: "" },
+                modifier = Modifier.padding(vertical = 8.dp),
+                singleLine = true,
+                textStyle = androidx.compose.ui.text.TextStyle(fontSize = 18.sp)
             )
         }
+
+        // Display "Climbing Style" text and input box in a Column
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // display "Full Name" text
+            Text(
+                text = "Climbing Style",
+                textAlign = TextAlign.Left,
+                fontSize = 18.sp
+            )
+
+
+            // dropdown menu of climbing styles
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = it }
+            ) {
+                TextField(
+                    value = climbStyle,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    modifier = Modifier.menuAnchor()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    climbStyles.forEach { style ->
+                        DropdownMenuItem(
+                            text = { Text(text = style) },
+                            onClick = { climbStyle = style }
+                        )
+                    }
+                }
+            }
+
+        }
+
         // button to let user sign out
-        Button(onClick = onSignOut) {
+        Button(
+            onClick = onSignOut,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        ) {
             Text(text = "Sign out")
         }
     }
-
 }
+
