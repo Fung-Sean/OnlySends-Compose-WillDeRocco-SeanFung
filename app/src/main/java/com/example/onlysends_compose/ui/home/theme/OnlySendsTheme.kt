9af3956e6f89f5f153f73e0e77e2dcd660550388
Fung-Sean <98567885+Fung-Sean@users.noreply.github.com>
2024-04-16 17:55:30 -0400

    Adding resources for later use

---
+++ app/src/main/java/com/example/onlysends_compose/ui/home/theme/OnlySendsTheme.kt
@@ -0,0 +1,20 @@
+package com.example.onlysends_compose.ui.home.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SocialAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    val colors = if (darkTheme) DarkColors else LightColors
+    MaterialTheme(
+        colors = colors,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
