9833fca704931427ce504532472c454e198e9f65
Fung-Sean <98567885+Fung-Sean@users.noreply.github.com>
2024-04-16 17:59:33 -0400

    Adding resources for later usage

--- app/src/main/java/com/example/onlysends_compose/ui/home/theme/Type.kt
+++ app/src/main/java/com/example/onlysends_compose/ui/home/theme/Type.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.onlysends_compose.R
+
 val Lexend = FontFamily(
     Font(R.font.lexend_medium, FontWeight.Medium),
     Font(R.font.lexend_semi_bold, FontWeight.SemiBold),
