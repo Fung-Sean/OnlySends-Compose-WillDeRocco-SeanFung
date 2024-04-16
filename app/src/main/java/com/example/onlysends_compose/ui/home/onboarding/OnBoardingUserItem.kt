c2d8ed10d10b54c737311bd6ae440a984a48b5c3
Fung-Sean <98567885+Fung-Sean@users.noreply.github.com>
2024-04-16 16:40:53 -0400

    Adding setup files for the homescreen and dummy data

---
+++ app/src/main/java/com/example/onlysends_compose/ui/home/onboarding/OnBoardingUserItem.kt
@@ -0,0 +1,15 @@
+package com.example.onlysends_compose.ui.home.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.onlysends_compose.ui.home.fake_data.FollowsUser
+
+@Composable
+fun OnBoardingUserItem(
+    modifier: Modifier = Modifier,
+    followsUser: FollowsUser,
+    onUserClick: (FollowsUser) -> Unit
+
+){
+
+}
