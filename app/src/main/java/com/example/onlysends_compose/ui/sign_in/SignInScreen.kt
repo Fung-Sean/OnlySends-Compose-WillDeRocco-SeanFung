43cf112f69fd40a6866af1db6cead89201b0e43d
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-04-17 15:59:29 -0400

    idk what happenning (i think just doo-doo wifi)

--- app/src/main/java/com/example/onlysends_compose/ui/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/onlysends_compose/ui/sign_in/SignInScreen.kt
@@ -4,10 +4,17 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +28,9 @@ fun SignInScreen(
     // lambda function to sign-in
     onSignInClick: () -> Unit
 ) {
+    // grab current context (for Toast)
     val context = LocalContext.current
+
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
             Toast.makeText(
@@ -32,14 +41,24 @@ fun SignInScreen(
         }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "Sign in with Google")
+    // Show loading indicator if potentialFriends is empty and loading is true
+    if (state.isLoading) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+
+    } else {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = onSignInClick) {
+                Text(text = "Sign in with Google")
+            }
         }
     }
 }
