a3afa2317e933fd37ea9812dc45cb72c517876a5
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-04-18 14:57:37 -0400

    FriendsScreen connected to navbar

--- app/src/main/java/com/example/onlysends_compose/firestore/modules/SearchFirestore.kt
+++ app/src/main/java/com/example/onlysends_compose/firestore/modules/SearchFirestore.kt
@@ -59,3 +59,42 @@ fun searchAllFriends(
             onFriendsLoaded(emptyList()) // Return empty list in case of failure
         }
 }
+
+// searchUserFriends : returns a list of Friend objects for USER friends
+fun searchUserFriends(
+    db: FirebaseFirestore,
+    user: User,
+    onFriendsLoaded: (List<Friend>) -> Unit
+) {
+    val usersCollection = db.collection("users")
+
+    usersCollection.get()
+        .addOnSuccessListener { querySnapshot ->
+            val friendsList = mutableListOf<Friend>()
+
+            // iterate over every user document
+            for (document in querySnapshot.documents) {
+                // obtain the list of friends this friend has
+                val friendUser = document.toObject<User>() ?: User()
+                val friends = friendUser.friends
+
+                // TO-DO: filter out user and current friends
+                val friend = userToFriend(document)
+
+                Log.d(TAG, "friends of friend are $friends")
+                // Filter out the `user` `user.friends` and `friend.friends` for user
+                if (friend.userId != user.userId &&
+                    !user.friends.any { it.userId == friend.userId } &&
+                    !friends.any{ it.userId == user.userId }
+                ) {
+                    friendsList.add(friend)
+                }
+            }
+
+            onFriendsLoaded(friendsList)
+        }
+        .addOnFailureListener { exception ->
+            Log.e(TAG, "Error searching all friends", exception)
+            onFriendsLoaded(emptyList()) // Return empty list in case of failure
+        }
+}
