dcaac2db4ff861f3393953185310f15ab89270db
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-05-02 10:28:16 -0400

    added placeholder to searchbar and fixed padding issues

--- app/src/main/java/com/example/onlysends_compose/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/onlysends_compose/ui/search/SearchScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.onlysends_compose.components.friends.SearchFriendItem
 import com.example.onlysends_compose.components.generic.CustomSearchBar
@@ -63,13 +65,15 @@ fun SearchScreen(
 
             CustomSearchBar(
                 searchQuery = searchQuery,
+                placeHolder = "Search friends by name",
                 maxLength = 50,
                 onUpdateSearch = updateSearchQuery
             )
 
             LazyColumn(
                 modifier = modifier
-                    .fillMaxSize(),
+                    .fillMaxSize()
+                    .padding(bottom = 60.dp),
             ){
                 items(
                     items = filteredFriends,
@@ -90,3 +94,14 @@ fun SearchScreen(
     }
 }
 
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    SearchScreen(
+        searchUiState = SearchUiState(),
+        fetchMoreData = {  },
+        onFollowFriend = { },
+        onAcceptFriend = { }
+    )
+}
