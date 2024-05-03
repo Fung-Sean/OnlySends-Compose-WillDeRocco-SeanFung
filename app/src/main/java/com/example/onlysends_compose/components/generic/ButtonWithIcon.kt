137dd98be0f41e251245387142e02ac3ec72dd67
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-05-02 23:09:06 -0400

    integrated ButtonWithIcon for ProfileScreen

--- app/src/main/java/com/example/onlysends_compose/components/generic/ButtonWithIcon.kt
+++ app/src/main/java/com/example/onlysends_compose/components/generic/ButtonWithIcon.kt
@@ -1,5 +1,6 @@
 package com.example.onlysends_compose.components.generic
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,13 +17,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ButtonWithIcon(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier, // primarily used to adjust button size
     text: String,
+    textStyle: TextStyle = MaterialTheme.typography.titleMedium,
     colors: ButtonColors = ButtonDefaults.buttonColors(
         containerColor = colorResource(id = com.example.onlysends_compose.R.color.onlySends),
         contentColor = colorResource(id = com.example.onlysends_compose.R.color.white)
@@ -33,24 +36,21 @@ fun ButtonWithIcon(
     Button(
         colors = colors,
         onClick = { onClick() },
+        modifier = modifier
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier
-                .fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = text,
-                style = MaterialTheme.typography.labelMedium.copy(
-                    fontSize = 18.sp,
-                ),
-                modifier = modifier.weight(1f)
+                style = textStyle,
             )
-            Spacer(modifier = modifier.padding(4.dp))
             Icon(
                 imageVector = icon,
                 contentDescription = "Icon button ($text)",
-                modifier = modifier.size(24.dp)
+                modifier = Modifier.size(24.dp)
             )
         }
     }
