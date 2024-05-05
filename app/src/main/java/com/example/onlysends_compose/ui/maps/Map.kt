2f0b593b087835638a108b6ffecc44cfd2c40afa
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-05-05 13:30:43 -0400

    added loader to GoogleMap

--- app/src/main/java/com/example/onlysends_compose/ui/maps/Map.kt
+++ app/src/main/java/com/example/onlysends_compose/ui/maps/Map.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -16,6 +17,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CenterFocusWeak
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.res.colorResource
@@ -69,7 +72,8 @@ fun MapDisplay(
     val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
 
     AnimatedContent(
-        viewModel.locationState, label = "Map"
+        viewModel.locationState, label = "Map",
+        contentAlignment = Alignment.Center
     ) { state ->
         when (state) {
             is LocationState.NoPermission -> {
@@ -80,7 +84,20 @@ fun MapDisplay(
             }
 
             is LocationState.LocationLoading -> {
-                Text("Loading Map")
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 200.dp),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator()
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text("Loading Map")
+                    }
+                }
             }
 
             is LocationState.Error -> {
